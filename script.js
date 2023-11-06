fde667451c33d3e358506b7f6bf468c1e8676fa6
Deepak Saini <deepaksainiofficial17@gmail.com>
2023-11-07 00:29:52 +0530

    frontend added

---
+++ script.js
@@ -0,0 +1,5 @@
+// alert("Under Construction.....")
+
+// document.querySelector('.hamburger-icon').addEventListener('click', function() {
+//     this.parentElement.classList.toggle('active');
+// });
