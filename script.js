393f902d1ab840c22e0a1c028078355d301d50bd
Deepak Saini <deepaksainiofficial17@gmail.com>
2023-11-08 07:27:02 +0530

    changes added

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 alert("Under Construction.....")
 
 function myFunction() {
-    var x = document.getElementById("myNavBar");
-    if (x.className === "navbar") {
-      x.className += " responsive";
-    } else {
-      x.className = "navbar";
-    }
+  var a = document.getElementById("smallNav");
+  if (a.className === "small-navbar") {
+    a.className += " on";
+  } else {
+    a.className = "small-navbar";
+  };
+
+  var x = document.getElementById("myNavBar");
+  if (x.className === "responsive-links") {
+    x.className += " on";
+  } else {
+    x.className = "responsive-links";
   }
+}
